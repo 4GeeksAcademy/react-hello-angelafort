9b001c749e26a307de3d3a4936bb03c6fea2c506
angelafort <139291628+angelafort@users.noreply.github.com>
2023-09-22 15:08:10 +0000

    simple counter done

---
+++ src/js/component/icons.js
@@ -0,0 +1,9 @@
+import fontawesome from '@fontawesome/fontawesome';
+
+import faClock from '@fontawesome/fontawesome-free-regular/faClock';
+
+fontawesome.config = {
+    autoReplaceSvd:'nest'
+};
+
+fontawesome.library.add(faClock);
